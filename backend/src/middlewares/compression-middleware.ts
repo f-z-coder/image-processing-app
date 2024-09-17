ccd6854d5e9297a5f90866e59503569a3072aa80
Faiz Shaikh <faizshaikh72740@gmail.com>
2024-09-18 00:36:33 +0530

    remove compression

--- backend/src/middlewares/compression-middleware.ts
+++
@@ -1,7 +0,0 @@
-import compression from "compression";
-
-export const compressionMiddleware = compression({
-  level: 5,
-  filter: () => true, // Force compression for all responses
-  threshold: 0, // Compress all sizes
-});
--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,4 +1,3 @@
 export * from "./cors-middleware.js";
 export * from "./error-handler-middleware.js";
 export * from "./multer-middleware.js";
-export * from "./compression-middleware.js";

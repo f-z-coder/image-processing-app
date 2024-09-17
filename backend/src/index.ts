d1f8c17f0b3e09a3b0158b71b014f433a17d8fb3
Faiz Shaikh <faizshaikh72740@gmail.com>
2024-09-17 22:55:13 +0530

    compress response

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,19 @@ import imageProcessingPreviewRouter from "@routes/image-processing-preview-route
 import { IMAGE_MANAGEMENT_BASE_URL } from "@consts/image-management-consts.js";
 import { IMAGE_PROCESSING_PREVIEW_BASE_URL } from "@consts/image-processing-preview-consts.js";
 import { ENV_CONST } from "@consts/env-const.js";
-import { corsMiddleware, errorHandlerMiddleware } from "@middlewares/index.js";
+import {
+  corsMiddleware,
+  compressionMiddleware,
+  errorHandlerMiddleware,
+} from "@middlewares/index.js";
 
 const app = express();
 
 // Use CORS middleware
 app.use(corsMiddleware);
+
+// Use compression middleware
+app.use(compressionMiddleware);
 
 app.use(express.json());
 
--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export * from "./cors-middleware.js";
 export * from "./error-handler-middleware.js";
 export * from "./multer-middleware.js";
+export * from "./compression-middleware.js";
